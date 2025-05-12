47e21aa5b19d2552791cb269364fe7fba7373763
Engraya <ahmadyakubuaya@gmail.com>
2025-05-12 05:30:16 +0100

    Added some assigness

--- src/lib/assignees.ts
+++ src/lib/assignees.ts
@@ -1,35 +1,64 @@
 import { Assignee } from "../types/tasksTypes";
   
-  export const mockAssignees: Assignee[] = [
-    {
-      id: 'e1a2cbe1-4c5f-4e65-91d3-0fb87993881f',
-      name: 'Alice Johnson',
-      email: 'alice@example.com',
-      avatar: 'https://i.pravatar.cc/150?img=1'
-    },
-    {
-      id: 'a7c913e0-719b-4e4e-94e7-41b08e879d25',
-      name: 'Bob Smith',
-      email: 'bob@example.com',
-      avatar: 'https://i.pravatar.cc/150?img=2'
-    },
-    {
-      id: 'b3fe0e8f-8c2f-4b49-a618-cdcdbb20e11c',
-      name: 'Carol Williams',
-      email: 'carol@example.com',
-      avatar: 'https://i.pravatar.cc/150?img=3'
-    },
-    {
-      id: 'c47e6ae2-e07d-4db1-85f1-2ec55e1b9a33',
-      name: 'David Brown',
-      email: 'david@example.com',
-      avatar: 'https://i.pravatar.cc/150?img=4'
-    },
-    {
-      id: 'd93d9f44-86df-4628-9c1d-9ad66a505ba0',
-      name: 'Eva Green',
-      email: 'eva@example.com',
-      avatar: 'https://i.pravatar.cc/150?img=5'
-    }
-  ];
-  
+export const mockAssignees: Assignee[] = [
+  {
+    id: 'e1a2cbe1-4c5f-4e65-91d3-0fb87993881f',
+    name: 'Mehmet Uludag',
+    email: 'alice@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=1'
+  },
+  {
+    id: 'a7c913e0-719b-4e4e-94e7-41b08e879d25',
+    name: 'Hulya Yilmaz',
+    email: 'bob@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=2'
+  },
+  {
+    id: 'b3fe0e8f-8c2f-4b49-a618-cdcdbb20e11c',
+    name: 'Carol Williams',
+    email: 'carol@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=3'
+  },
+  {
+    id: 'c47e6ae2-e07d-4db1-85f1-2ec55e1b9a33',
+    name: 'Kemal Demir',
+    email: 'david@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=4'
+  },
+  {
+    id: 'd93d9f44-86df-4628-9c1d-9ad66a505ba0',
+    name: 'Eva Green',
+    email: 'eva@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=5'
+  },
+  {
+    id: 'e42aa91b-96db-4934-a65d-812e56923241',
+    name: 'Tolu Adebayo',
+    email: 'tolu@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=6'
+  },
+  {
+    id: 'f7d62177-2893-42e7-a1b3-2710d8b5f6ef',
+    name: 'Anna Kowalska',
+    email: 'anna@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=7'
+  },
+  {
+    id: 'af36f62d-76e3-48f6-ae76-529e4cfe9d70',
+    name: 'Ahmad Ali',
+    email: 'ahmed@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=8'
+  },
+  {
+    id: 'c99d045c-9f2a-4e23-89c9-1d543f2f559b',
+    name: 'Elif Kaya',
+    email: 'elif@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=9'
+  },
+  {
+    id: 'b1f3bb34-2b47-496e-b6a4-fb486ac7e914',
+    name: 'John Doe',
+    email: 'john@example.com',
+    avatar: 'https://i.pravatar.cc/150?img=10'
+  }
+];
